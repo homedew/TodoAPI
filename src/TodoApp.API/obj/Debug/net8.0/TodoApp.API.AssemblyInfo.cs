0919ceeb8518c344aab4b3acf0f7c0f79fa570aa
homedew <homedew.tc@gmail.com>
2025-04-23 12:25:08 +0700

    Update project structure

--- src/TodoApp.API/obj/Debug/net8.0/TodoApp.API.AssemblyInfo.cs
+++ src/TodoApp.API/obj/Debug/net8.0/TodoApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9e5f65725a2ab7e677a5388476472dab7a717c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37ff796b7c519c2b8a1a267949aab91d77916e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
